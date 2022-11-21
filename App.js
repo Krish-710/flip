020062fa8b5d35d6227d25c2596e7de5525ebd2d
Krishna Kriti <64309798+Krish-710@users.noreply.github.com>
2022-11-21 14:01:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/footer'
+import Flipflop from './components/flipflop'
+
+function App() {
+  return (
+    <div className="App">
+      <Flipflop/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ flipflop.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Flipflop = () => (
+    <div class="row">
+  <div class="column">
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <h>Click to Flip</h>
+      </div>
+      <div class="flip-card-back">
+        <h1>John Doe</h1>
+        <p>Architect & Engineer</p>
+        <p>We love that guy</p>
+      </div>
+    </div>
+  </div> 
+  </div>
+  <div class="column">
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <h>Click to Flip</h>
+      </div>
+      <div class="flip-card-back">
+        <h1>John Doe</h1>
+        <p>Architect & Engineer</p>
+        <p>We love that guy</p>
+      </div>
+    </div>
+  </div> 
+  </div>
+</div>
+    
+);
+
+export default Flipflop;
